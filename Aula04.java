7f0f53e5417f729cd9c6049dac68e0b22502b9b5
viniciusd7 <viniciusdm967@gmail.com>
2025-05-15 16:44:37 -0300

    incluindo mudanças

---
+++ Aula04.java
@@ -0,0 +1,17 @@
+public class Aula04 {
+    public static void main(String[] args) {
+        // entendendo nomeclatura de variaveis
+
+        double salarioMinimo = 2.500;
+        int  numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; 
+
+        int numero = 1;
+        long numeroGrande = 1000000000000L; // L para indicar que é um long
+        float numeroFlutuante = 10.5f; // f para indicar que é um float
+        double numeroDouble = 10.5; // sem sufixo, é considerado double por padrão
+        boolean verdadeiro = true; // ou false
+        char letra = 'A'; // aspas simples para char
+        String texto = "Olá, mundo!"; // aspas duplas para String
+    }
+}
---
+++ Aula05.java
@@ -0,0 +1,6 @@
+public class Aula05 { 
+    public static void main(String[] args) {
+        String nomecompleto = "vinicius" + "Diniz";
+        System.out.println(nomecompleto);
+    }
+}
